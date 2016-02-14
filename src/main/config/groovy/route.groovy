fb268bd644ec5c47f0bc71f37f4f2177f1cc70c7
kazamage <kazamage1980@gmail.com>
2016-02-15 00:59:54 +0900

    refactoring.

--- src/main/config/groovy/route.groovy
+++ src/main/config/groovy/route.groovy
@@ -4,6 +4,8 @@ import jp.pgw.develop.swallow.tool.GenericRoute
 import jp.pgw.develop.swallow.tool.RouteDefinition
 
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.Exchange
+import org.apache.camel.processor.aggregate.AggregationStrategy
 
 beans {
   routes(GenericRoute, { RouteBuilder builder ->
@@ -17,15 +19,29 @@ beans {
 //      .to('jms:queue:in?includeSentJMSMessageID=true')
 //      .log('\${in.header.JMSMessageId}')
     builder
-      .from('file:C:/dev/data/?fileName=input.txt')
+      .from('file:C:/dev/data/?recursive=true')
       .routeId('fileToIn')
+      .log('\ninput body:\n\${body}')
       .to('jms:queue:in?includeSentJMSMessageID=true')
-      .log('to in  : \${in.header.JMSMessageId}')
+      .setHeader('inId', builder.simple('\${header.JMSMessageId}'))
+      .pollEnrich("jms:queue:out?includeSentJMSMessageID=true", { Exchange original, Exchange resource ->
+          original.getIn().setBody(resource.getIn().getBody())
+          original.getIn().headers.put('outId', resource.getIn().getHeader("JMSMessageId"))
+          return original
+      } as AggregationStrategy)
+      .pollEnrich('file:C:/dev/diff/?fileName=\${header.CamelFileName}&readLockMarkerFile=false', { Exchange original, Exchange resource ->
+          String expected = resource.getIn().getBody(String)
+          String actual = original.getIn().getBody(String)
+          original.getIn().setHeader("expected", expected)
+          original.getIn().setHeader("compResult", java.util.Objects.equals(expected, actual))
+          return original
+      } as AggregationStrategy)
+      .log('\ninput id:\n\${header.inId}\noutput id:\n\${header.outId}\nexpected body:\n\${header.expected}\nactual body:\n\${body}\ncompResult:\n\${header.compResult}')
     builder
       .from('jms:queue:in')
       .routeId('inToOut')
-      .log('from in: \${in.header.JMSMessageId}')
+      .log('from in: \${header.JMSMessageId}')
       .to('jms:queue:out?includeSentJMSMessageID=true')
-      .log('to out : \${in.header.JMSMessageId}')
+      .log('to out : \${header.JMSMessageId}')
   } as RouteDefinition)
 }
